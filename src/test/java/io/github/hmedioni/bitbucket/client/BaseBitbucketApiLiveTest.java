169a1d067c37aa6d04ee7e5622c12fdb858e744d
hillel <hillel95med@gmail.com>
2023-09-06 13:20:36 +0300

    fix test and create new ep

--- src/test/java/io/github/hmedioni/bitbucket/client/BaseBitbucketApiLiveTest.java
+++ src/test/java/io/github/hmedioni/bitbucket/client/BaseBitbucketApiLiveTest.java
@@ -11,8 +11,8 @@ public class BaseBitbucketApiLiveTest extends AbstractTestNGSpringContextTests {
     protected final String defaultBitbucketGroup = "stash-users";
     final protected ObjectMapper objectMapper = new ObjectMapper();
     final protected String url = "http://127.0.0.1:7990";
-    final private String user = "hillelMed";
-    final private String password = "hh15975352";
+    final private String user = "admin";
+    final private String password = "password";
     protected BitbucketProperties bitbucketProperties = new BitbucketProperties(url, user, password);
     protected BitbucketClient bitbucketClient = BitbucketClient.create(bitbucketProperties);
     protected BitbucketApi api = bitbucketClient.api();
